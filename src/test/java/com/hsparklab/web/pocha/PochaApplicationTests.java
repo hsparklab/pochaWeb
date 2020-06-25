122920e160af87df38ccec3f082bfcf627b608b9
hyeonseokPark <hyeonseok.park7@gmail.com>
2020-06-25 17:14:36 +0900

    first

---
+++ src/test/java/com/hsparklab/web/pocha/PochaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsparklab.web.pocha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PochaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
